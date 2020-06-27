d895dab3c0ea3b5c8f8e1a6ec68f417c3857266c
xiaoxigua-1 <60529600+xiaoxigua-1@users.noreply.github.com>
2020-06-27 21:15:39 +0800

    Update apple.py

--- apple.py
+++ apple.py
@@ -1,10 +1,44 @@
-from flask import Flask,request
+from flask import Flask
+import flask
+import os
+import random
+import subprocess
+import zipfile
+import shutil
 app = Flask(__name__)
-@app.route("/")
+playlist=[]
+@app.route("/dl/")
 def pp():
-    print("p")
-    return "87"
-def main():#????
+    w2=flask.request.args.get("filename")
+    try :
+        w=flask.send_file(f"dl\\{w2}.mp3")
+    except :
+        w=flask.send_file(f"dl\\{w2}.zip")
+    return  w
+@app.route("/spotify/")
+def spotify():
+    url=flask.request.args.get("url")
+    dl=os.listdir("dl")
+    filename=flask.request.args.get("filename")
+    if filename in dl :
+        return '重新輸入名稱'
+    #下載spotift歌曲
+    if str(url).startswith("https://open.spotify.com/track/"):
+        cmd="spotdl -f dl\\%s.{output-ext} --song %s"%(filename,url)
+        subprocess.call(cmd, shell=True) 
+    elif str(url).startswith("https://open.spotify.com/playlist"):
+        cmd=f"spotdl --write-to dl\\{filename}.txt --playlist {url}"
+        subprocess.call(cmd, shell=True)
+        os.mkdir(f'dl\\{str(filename)}')
+        cmd="spotdl -f dl\\%s\\{track-name}.{output-ext} --list dl\\%s.txt"%(filename,filename)
+        subprocess.call(cmd, shell=True)
+        os.remove(f"dl\\{filename}.txt")
+        zipf=zipfile.ZipFile(f"dl\\{filename}.zip","w")
+        dlp=os.listdir(f"dl\\{filename}")
+        for p in dlp :
+            zipf.write('dl\\%s\\%s'%(filename,p))
+        shutil.rmtree(f"dl\\{filename}")
+def main():
     if __name__ == "__main__":
         print("開啟中")
         app.run()
