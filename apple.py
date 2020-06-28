b5c4f7e5877878bb3b8535c584274b37b1967fc6
xiaoxigua-1 <60529600+xiaoxigua-1@users.noreply.github.com>
2020-06-28 13:03:19 +0800

    Update apple.py

--- apple.py
+++ apple.py
@@ -6,15 +6,6 @@ import subprocess
 import zipfile
 import shutil
 app = Flask(__name__)
-playlist=[]
-@app.route("/dl/")
-def pp():
-    w2=flask.request.args.get("filename")
-    try :
-        w=flask.send_file(f"dl\\{w2}.mp3")
-    except :
-        w=flask.send_file(f"dl\\{w2}.zip")
-    return  w
 @app.route("/spotify/")
 def spotify():
     url=flask.request.args.get("url")
@@ -26,6 +17,8 @@ def spotify():
     if str(url).startswith("https://open.spotify.com/track/"):
         cmd="spotdl -f dl\\%s.{output-ext} --song %s"%(filename,url)
         subprocess.call(cmd, shell=True) 
+        w=flask.send_file(f"dl\\{w2}.mp3")
+        return  w
     elif str(url).startswith("https://open.spotify.com/playlist"):
         cmd=f"spotdl --write-to dl\\{filename}.txt --playlist {url}"
         subprocess.call(cmd, shell=True)
@@ -38,6 +31,8 @@ def spotify():
         for p in dlp :
             zipf.write('dl\\%s\\%s'%(filename,p))
         shutil.rmtree(f"dl\\{filename}")
+        w=flask.send_file(f"dl\\{w2}.zip")
+        return w
 def main():
     if __name__ == "__main__":
         print("開啟中")
