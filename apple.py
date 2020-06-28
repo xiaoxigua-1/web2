ad050e16e0bea6ae9cbbc2d6781c7055b78470ce
xiaoxigua-1 <60529600+xiaoxigua-1@users.noreply.github.com>
2020-06-28 13:37:10 +0800

    Update apple.py

--- apple.py
+++ apple.py
@@ -15,15 +15,15 @@ def spotify():
         return '重新輸入名稱'
     #下載spotift歌曲
     if str(url).startswith("https://open.spotify.com/track/"):
-        cmd="spotdl -f dl/%s.{output-ext} --song %s"%(filename,url)
+        cmd="spotdl -o {mp3} -f dl/%s.{output-ext} --song %s"%(filename,url)
         subprocess.call(cmd, shell=True) 
-        w=flask.send_file(f"dl/{filename}.opus")
+        w=flask.send_file(f"dl/{filename}.mp3")
         return  w
     elif str(url).startswith("https://open.spotify.com/playlist"):
         cmd=f"spotdl --write-to dl/{filename}.txt --playlist {url}"
         subprocess.call(cmd, shell=True)
         os.mkdir(f'dl/{str(filename)}')
-        cmd="spotdl -f dl/%s/{track-name}.{output-ext} --list dl/%s.txt"%(filename,filename)
+        cmd="spotdl -o {mp3} -f dl/%s/{track-name}.{output-ext} --list dl/%s.txt"%(filename,filename)
         subprocess.call(cmd, shell=True)
         os.remove(f"dl/{filename}.txt")
         zipf=zipfile.ZipFile(f"dl/{filename}.zip","w")
